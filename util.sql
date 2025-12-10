c756fa90598d9c57233f95805579240f52f487bc
José Melo <jos.ryan.melo@gmail.com>
2025-12-09 22:35:23 -0300

    .

---
+++ util.sql
@@ -0,0 +1,20 @@
+-- DROP OWNED BY CURRENT USER;
+
+-- \dn
+
+-- show search_path;
+
+-- set search_path to company, public;
+
+-- \dt public.*      mostra todas as tabelas do schema public
+
+
+-- Setta permanentemente o search_path do usuario
+-- alter database youruser_db set search_path to company, public;
+-- set search_path to company, public;
+
+
+-- \i company.sql      carrega tudo de company
+
+
+
